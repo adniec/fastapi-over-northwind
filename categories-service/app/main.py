6f271730b0e29997ade5fa3d21406852053944cd
etzior <ethru@protonmail.com>
2020-10-27 20:23:25 +0100

    Add categories router

--- categories-service/app/main.py
+++ categories-service/app/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 
+from app.api.categories import categories
 from app.api.db import database, engine, metadata
 
 metadata.create_all(engine)
 
 app = FastAPI(openapi_url="/api/categories/openapi.json", docs_url="/api/categories/docs")
 
+
 @app.on_event("startup")
 async def startup():
     await database.connect()
 
+
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+
+app.include_router(categories, prefix='/api/categories', tags=['categories'])
