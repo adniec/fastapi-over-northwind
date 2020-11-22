200e9ba4504dc5f4acd3b97fc18395027cc44d0a
ethru <ethru@protonmail.com>
2020-11-22 12:16:58 +0100

    Add product search

--- products-service/app/api/db.py
+++ products-service/app/api/db.py
@@ -24,3 +24,16 @@ products = Table(
 )
 
 database = Database(DATABASE_URI)
+
+
+async def search(payload: dict):
+    if not payload:
+        return await database.fetch_all(query=products.select())
+
+    payload = {k: f"'{v}'" if isinstance(v, str) else v for k, v in payload.items()}
+    condition = ' AND '.join([f'{k}={v}' for k, v in payload.items()])
+    query = f'SELECT * FROM products WHERE {condition}'
+    result = []
+    async for row in database.iterate(query=query):
+        result.append(row)
+    return result
--- products-service/app/api/products.py
+++ products-service/app/api/products.py
@@ -5,6 +5,9 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import HTTPBasicCredentials, HTTPBearer
 
+from app.api import db
+from app.api.models import Product
+
 products = APIRouter()
 security = HTTPBearer()
 
@@ -13,3 +16,9 @@ def authorize(credentials: HTTPBasicCredentials = Depends(security)):
     response = httpx.get(os.getenv('AUTH_SERVICE_URL'), headers={'Authorization': credentials.credentials})
     if response.status_code == 401:
         raise HTTPException(status_code=401)
+
+
+@products.post('/search')
+async def search(payload: Product):
+    parameters = {k: v for k, v in payload.dict().items() if v is not None}
+    return await db.search(parameters)
