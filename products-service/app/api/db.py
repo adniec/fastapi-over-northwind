3822b6f30aa24c606c02e0a4dba12a9d3b60b863
ethru <ethru@protonmail.com>
2021-01-08 02:58:04 +0100

    Extend products service db functions

--- products-service/app/api/db.py
+++ products-service/app/api/db.py
@@ -1,9 +1,20 @@
-from app.api import database
+from asyncpg.exceptions import ForeignKeyViolationError
+from fastapi import HTTPException
 
-from app.api.models import products
+from app.api import database
+from app.api.models import ProductIn, ProductOut, products
+
+
+async def get_product(product_id: int):
+    """Get product with set id from database."""
+    return await database.fetch_one(query=products.select().where(products.c.product_id == product_id))
 
 
 async def search(payload: dict):
+    """Get products matching payload from database.
+
+    If there is no payload return all products.
+    """
     if not payload:
         return await database.fetch_all(query=products.select())
 
@@ -14,3 +25,32 @@ async def search(payload: dict):
     async for row in database.iterate(query=query):
         result.append(row)
     return result
+
+
+async def add_product(payload: ProductIn):
+    """Store new product in database."""
+    query = products.insert().values(**payload.dict())
+    try:
+        return await database.execute(query=query)
+    except ForeignKeyViolationError:
+        raise HTTPException(status_code=422, detail='Wrong foreign key. Record with set id not in database.')
+
+
+async def update(payload: ProductOut):
+    """Update product with set id in database."""
+    query = products.update().where(
+        products.c.product_id == payload.product_id
+    ).values(**payload.dict()).returning(products)
+    try:
+        return await database.execute(query=query)
+    except ForeignKeyViolationError:
+        raise HTTPException(status_code=422, detail='Wrong foreign key. Record with set id not in database.')
+
+
+async def delete(product_id: int):
+    """Remove product with set id from database.
+
+    All linked orders and order_details are also deleted (cascade).
+    """
+    query = products.delete().where(products.c.product_id == product_id).returning(products)
+    return await database.execute(query=query)
